4f261a4a45d560b365d768a5fb3ff1bb064953bb
Pzqqt <821026875@qq.com>
2022-09-29 15:27:44 +0800

    drivers: nt36xxx: Remove broken debug message

--- drivers/input/touchscreen/nt36672c/nt36xxx.c
+++ drivers/input/touchscreen/nt36672c/nt36xxx.c
@@ -3032,7 +3032,6 @@ static int nvt_drm_notifier_callback(struct notifier_block *self, unsigned long 
 
 	if (evdata && ts_data) {
 		blank = evdata->data;
-		NVT_LOG("%s: event:%lu,blank:%u\n", event, blank);
 
 		if (event == MSM_DRM_EARLY_EVENT_BLANK) {
 			if (*blank == MSM_DRM_BLANK_POWERDOWN) {
